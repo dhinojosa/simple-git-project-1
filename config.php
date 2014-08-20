76097f9c5f6a97c525ea475c1730d087a7871e9e
Jim Dryden <jdryden@unm.edu>
2014-08-20 10:29:31 -0600

    edited config.php with password and user

--- config.php
+++ config.php
@@ -3,6 +3,8 @@
 $today = now();
 $tomorrow = "is just tomorrow";
 $nextday = $today + 24;
+$user = 'admin';
+$password = 'r1ckjame5';
 
 echo now();
 	echo "<p>Here is some info about my system:</p>";
